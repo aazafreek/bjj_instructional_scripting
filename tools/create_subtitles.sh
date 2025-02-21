543059a0734ec54741d3478977d96cc248168c8b
abaratham <anandhiya.baratham@gmail.com>
2025-02-21 15:40:31 -0500

    no message

--- tools/create_subtitles.sh
+++ tools/create_subtitles.sh
@@ -9,31 +9,31 @@ log_error() {
 }
 
 video_dir=$1
-data_dir=$2
+output_dir=$2
 
 video_files=$(ls $video_dir | grep '.mp4')
 cd $video_dir
-srt_files=""
+file_names=""
 for file in $video_files; do
 	file_name=$( echo $file | cut -d'.' -f1 )
-	if [ ! -f "$data_dir/$file_name.srt" ] && [ ! -f "$video_dir/$file_name.srt" ]; then
-		srt_files="$file $srt_files"
+	if [ ! -f "$output_dir/$file_name.srt" ] && [ ! -f "$video_dir/$file_name.srt" ]; then
+		file_names="$file_name $file_names"
 	fi
 done
 
 unset $0
 
-for file in $srt_files; do
-	npx gen-subs for $file && echo -e  "\e[32m[INFO]\e[0m Created subtitles for $file."
+for file in $file_names; do
+	npx gen-subs for $file.mp4 && echo -e  "\e[32m[INFO]\e[0m Created subtitles for $file."
 done
 # echo $srt_files | xargs -n 1 -P 4 bash -c 'npx gen-subs for "$0" &> $0.log && echo -e "\e[32m[INFO]\e[0m Created subtitles for $0."'
 
-for file in $srt_files; do
+for file in $file_names; do
 	file_name=$( echo $file | cut -d'.' -f1 )
-	if [ ! -f "$data_dir/$file_name.srt" ] && [ ! -f "$video_dir/$file_name.srt" ]; then
+	if [ ! -f "$output_dir/$file_name.srt" ] && [ ! -f "$video_dir/$file_name.srt" ]; then
 		log_error "ERROR: Enable to create subtitles for $file_name"
 	fi
 done
 
-mv $video_dir/*.srt $data_dir 2> /dev/null
-mv $video_dir/*.log $data_dir 2> /dev/null
+mv $video_dir/*.srt $output_dir 2> /dev/null
+mv $video_dir/*.log $output_dir 2> /dev/null
