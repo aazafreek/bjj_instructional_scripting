3dabb91c537e8830c3ba9b02e883cb7e14ffb51d
abaratham <anandhiya.baratham@gmail.com>
2025-02-21 15:49:19 -0500

    no message

--- tools/clean_subtitles.sh
+++ tools/clean_subtitles.sh
@@ -4,14 +4,16 @@
 data_dir=$1
 cd $data_dir
 for file in $( ls *.srt ); do
-	i=0
-	IFS=$'\n'
-	for line in $( cat $file ); do
-		mod=$(( $i%3 ))
-		if (( $mod == 2 )); then
-			echo $line >> $file.cln
-		fi
-		((i++))
-	done
-	unset IFS
+	if [ ! -f $file.cln ]; then
+		i=0
+		IFS=$'\n'
+		for line in $( cat $file ); do
+			mod=$(( $i%3 ))
+			if (( $mod == 2 )); then
+				echo $line >> $file.cln
+			fi
+			((i++))
+		done
+		unset IFS
+	fi
 done
