e6aa94f3d2c31b6e07ab685bb27e464b23d0023b
Anand <anandhiya.baratham@gmail.com>
2025-02-21 13:00:21 -0500

    asdf

--- scripts/refresh-patterns.sh
+++ scripts/refresh-patterns.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 
-REPO_BASE_PATH=${1:.}
+REPO_BASE_PATH=${1:-.}
 
-rm -rf ~/.config/fabric/patterns
+rm -rf ~/.config/fabric/patterns/*
 
 cp -r $REPO_BASE_PATH/patterns/* ~/.config/fabric/patterns
 
 fabric --updatepatterns
+fabric -l
