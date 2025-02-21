9846ebfc8d24c86370f6beb2825562ff53c02aa9
Anand <anandhiya.baratham@gmail.com>
2025-02-21 13:03:44 -0500

    asdf

--- obtener_modelo.sh
+++
@@ -1,4 +0,0 @@
-[ ! -d ./model ] && mkdir ./model/
-wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -o ./model/vosk-model.zip
-mv model/vosk-model.zip /tmp/gen-subs/models/
-unzip /tmp/gen-subs/models/*.zip
